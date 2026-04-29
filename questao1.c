6f0af53b46dfd4950bcd4a39155f3cc6b53350ae
Mariana <sousa06390@gmail.com>
2026-04-29 09:16:35 -0300

    Delete questao1.c

--- questao1.c
+++
@@ -1,8 +0,0 @@
-/*Questão 01
-Implemente uma função trocar(int *a, int *b) que receba duas variáveis inteiras por
-referência e troque seus valores entre si, sem utilizar variáveis globais.*/
-#include <stdio.h>
-#include "questao1.h"
-int trocar(int *a, int*b){
-    return 0;
-}
