afb368239ce069c3f7a4f0384d87c6d1fd200a0e
Mariana <sousa06390@gmail.com>
2026-05-05 09:49:17 -0300

    Delete Introdução a Programação/matematica.h

--- Introdução a Programação/matematica.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATEMATICA
-#define MATEMATICA
-int equacao(float a, float b, float c, float *raiz1, 
-    float *raiz2);
-int soma(int a, int b);
-int fatorial(int n);
-int numeroArranjos(int n , int s);
-#endif 
