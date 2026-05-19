7db85423de34915d135240451509dd8fea4c5ced
Mariana Siqueira <sousa06390@gmail.com>
2026-05-19 16:21:04 -0300

    Organização Inicial

---
+++ Introdução a Programação/Trabalho/memoria.c
@@ -0,0 +1,43 @@
+void exibirMemoria(int memoria[], int tamanho){
+
+}
+
+int alocarFirstFit(int memoria[],int tamanho,int pid,int quantidade){
+
+}
+
+int alocarBestFit(int memoria[],int tamanho,int pid,int quantidade){
+
+}
+
+int alocarWorstFit(int memoria[],int tamanho,int pid,int quantidade){
+
+}
+
+int liberarProcesso(int memoria[],int tamanho,int pid){
+
+}
+
+void compactarMemoria(int memoria[],int tamanho){
+
+}
+
+void relatorioMemoria(int memoria[],int tamanho){
+
+}
+
+void relatorioEficiencia(int alocacoes[],int desperdicios[]){
+
+}
+
+int maiorBlocoLivre(int memoria[],int tamanho){
+
+}
+
+int contarFragmentos(int memoria[],int tamanho){
+
+}
+
+int buscarPID(int memoria[],int tamanho,int pid){
+    
+}
